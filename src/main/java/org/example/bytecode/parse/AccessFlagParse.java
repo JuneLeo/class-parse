33bea452e66581d80d219456c2c88b359befd623
曉豹 <xiaobao.spf@alibaba-inc.com>
2023-01-13 21:48:30 +0800

    access flag

--- src/main/java/org/example/bytecode/parse/AccessFlagParse.java
+++ src/main/java/org/example/bytecode/parse/AccessFlagParse.java
@@ -3,13 +3,26 @@ package org.example.bytecode.parse;
 import org.example.bytecode.parse.constant.Parse;
 import org.example.bytecode.Utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AccessFlagParse implements Parse {
-    public int accessFlag;
+    public int value;
+    public List<String> accessFlags = new ArrayList<>();
 
     @Override
     public int parse(int start, byte[] bytes) {
-        accessFlag = Utils.getU2Int(start, bytes);
+        value = Utils.getU2Int(start, bytes);
+        format();
         start += 2;
         return start;
+    }
+
+    private void format() {
+        for (AccessFlag access : AccessFlag.values()) {
+            if ((access.value & value) > 0) {
+                accessFlags.add(access.name());
+            }
+        }
     }
 }
